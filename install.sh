421c8ede9b8c4d0676ab68ea27b81f34ac53faeb
Siddharth <siddharth99c@gmail.com>
2024-02-02 22:20:36 +0530

    feat: isolate macos code

--- install.sh
+++ install.sh
@@ -4,10 +4,14 @@
 # 2. Tweak macOS preferences
 # 3. Install apps
 
-# Function to install brew packages
+# Function to install packages
 install_packages() {
   for pkg in "$@"; do
-    brew install "$pkg"
+    if [ "$(uname)" == "Darwin" ]; then
+      brew install "$pkg" # macOS
+    else
+      sudo apt install "$pkg" # Linux
+    fi
     if [ $? -eq 0 ]; then
       echo "✅ $pkg installed"
       echo "--------------------"
@@ -36,17 +40,41 @@ install_apps() {
 # 1. Setup development environment 
 ####################################
 
-# Install Homebrew
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+# Install Homebrew (macOS only)
+if [ "$(uname)" == "Darwin" ]; then
+  if ! [ -x "$(command -v brew)" ]; then
+    echo "❌ Installing Homebrew..."
+    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
-# Create .zprofile and add Homebrew to it
-touch ~/.zprofile
-echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/siddharth/.zprofile
-eval "$(/opt/homebrew/bin/brew shellenv)"
-echo '✅ brew installed'
-echo "--------------------\n"
+    # Create .zprofile and add Homebrew to it
+    touch ~/.zprofile
+    echo 'eval "$(/opt/homebrew/bin/brew shellenv)"' >> /Users/siddharth/.zprofile
+    eval "$(/opt/homebrew/bin/brew shellenv)"
+  fi
+  echo '✅ Homebrew installed'
+  echo "--------------------\n"
+fi
 
-# Install useful packages
+# Install Git if not installed
+if ! [ -x "$(command -v git)" ]; then
+  echo "❌ git is not installed. Installing Git..."  
+  install_packages git # Install Git
+else
+  echo "✅ git is already installed"
+fi
+
+# Install Zsh if not installed
+if ! [ -x "$(command -v zsh)" ]; then
+  echo "❌ zsh is not installed. Installing Zsh..."
+  install_packages zsh # Install Zsh
+else
+  echo "✅ zsh is already installed"
+fi
+
+# Install Rust
+curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh
+
+# Install other packages
 install_packages neofetch sl
 
 # Install FNM (Fast Node Manager)
@@ -66,47 +94,52 @@ npm install yarn -g
 # Move .zshrc to ~
 mv ./.zshrc ~
 
-# Configure Git
 
 
 ####################################
 # 2. Tweak macOS Preferences
 ####################################
 
-# Remove login message in Apple Terminal
-touch ~/.hushlogin
+if [ "$(uname)" == "Darwin" ]; then
+  # Remove login message in Apple Terminal
+  touch ~/.hushlogin
 
-# Tweak macOS preferences
-defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
-echo "✅ Disabled automatic spelling correction"
+  # Tweak macOS preferences
+  defaults write NSGlobalDomain NSAutomaticSpellingCorrectionEnabled -bool false
+  echo "✅ Disabled automatic spelling correction"
 
-defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
-echo "✅ Disabled automatic capitalization"
+  defaults write NSGlobalDomain NSAutomaticCapitalizationEnabled -bool false
+  echo "✅ Disabled automatic capitalization"
 
-defaults write com.apple.menuextra.battery ShowPercent -bool true
-echo "✅ Battery percentage display enabled"
+  defaults write com.apple.menuextra.battery ShowPercent -bool true
+  echo "✅ Battery percentage display enabled"
 
-defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
-echo "✅ 3-finger drag gesture enabled"
+  defaults write com.apple.AppleMultitouchTrackpad TrackpadThreeFingerDrag -bool true
+  echo "✅ 3-finger drag gesture enabled"
+fi
 
 
 ####################################
 # 3. Install apps  
 ####################################
 
-install_apps \
-  visual-studio-code \
-  raycast \
-  handbrake \
-  iina \
-  the-unarchiver \
-  mouse-fix \
-  monitorcontrol \
-  spitfire-audio \
-  ableton-live-standard \
-  figma \
-  framer \
-  blender \
-  affinity-designer \
-  affinity-photo \
-  obsidian
+# run the following only on macos
+if [ "$(uname)" == "Darwin" ]; then
+  # Install apps
+  install_apps \
+    visual-studio-code \
+    raycast \
+    handbrake \
+    iina \
+    the-unarchiver \
+    mouse-fix \
+    monitorcontrol \
+    spitfire-audio \
+    ableton-live-standard \
+    figma \
+    framer \
+    blender \
+    affinity-designer \
+    affinity-photo \
+    obsidian
+fi
